6fa0c0bc0365dc53af2e3dace345d5e25b2bfe90
Petar Dambovaliev <petar.atanasov.1987@gmail.com>
2021-01-13 07:54:17 +0100

    save

--- darpi-web/src/lib.rs
+++ darpi-web/src/lib.rs
@@ -4,6 +4,9 @@ pub mod logger;
 pub mod middleware;
 pub mod request;
 pub mod response;
+mod xml;
+mod yaml;
+
 pub use hyper::{body::HttpBody, Body, Request, Response, StatusCode};
 pub use json::Json;
 pub use request::Query;
